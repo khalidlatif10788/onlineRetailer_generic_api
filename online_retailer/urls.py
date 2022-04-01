4871e2aa70ab5e3080cc5d955c546aa7a9f8a06e
Khalid <realism10788@gmail.com>
2022-04-01 12:31:13 +0500

    model View Set Api approach

--- online_retailer/urls.py
+++ online_retailer/urls.py
@@ -1,21 +1,21 @@
 
 from django.conf import settings
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 from django.conf.urls.static import static
 from online_retailer import views
+from django.urls import path,include
 from online_retailer.views import productList,productUpdateView,productDeleteView
 
+# create object  Default Router class
+router=DefaultRouter()
+# register class 
+router.register('track',views.productModelviewset,basename='Product')
 urlpatterns = [
     path('', productList.as_view(),name="ho"),
      path('<pk>/update', productUpdateView.as_view(),name="Product_update"),
     path('<pk>/delete/', productDeleteView.as_view(),name="Product_delete"),
-    # api product retrive url
-    path('productList/',views.prodcutList.as_view(), name="productList" ),
-    path('productReteriveSingle/<int:pk>',views.productRetrieve.as_view(), name="productSingle"),
-    # api product update
-     path('productUpdate/<int:pk>',views.productUpdateView.as_view(), name="productUpdate"),
-     #
-    path('productDelete/<int:pk>',views.productDestroy.as_view(), name="productDelete"),
-    #
+    # ModelViewSet Api
+     path('apimodelViewSet/',include(router.urls), name="apimodelviewSet"),
   
 ]+ static(settings.MEDIA_URL,document_root= settings.MEDIA_ROOT)
--- online_retailer/views.py
+++ online_retailer/views.py
@@ -34,41 +34,9 @@ class productDeleteView(DeleteView):
 # Api code
 
 
-from rest_framework.generics import GenericAPIView
-from rest_framework.mixins import ListModelMixin, CreateModelMixin,RetrieveModelMixin,UpdateModelMixin,DestroyModelMixin
 
+from rest_framework import viewsets
 
-class prodcutList(GenericAPIView,ListModelMixin):
+class productModelviewset(viewsets.ModelViewSet):
     queryset = product.objects.all()
     serializer_class = productSerializer
-
-    def get(self,request,*args,**kwargs):
-        return self.list(request,*args,**kwargs)
-
-class prodcutCreate(GenericAPIView,CreateModelMixin):
-    queryset = product.objects.all()
-    serializer_class = productSerializer
-
-    def post(self,request,*args,**kwargs):
-        return self.create(request,*args,**kwargs)
-
-class productRetrieve(GenericAPIView,RetrieveModelMixin):
-    queryset = product.objects.all()
-    serializer_class = productSerializer
-
-    def get(self,request,*args,**kwargs):
-        return self.retrieve(request,*args,**kwargs)
-
-
-class productUpdate(GenericAPIView,UpdateModelMixin ):
-    queryset = product.objects.all()
-    serializer_class = productSerializer
-
-    def put(self,request,*args,**kwargs):
-        return self.update(request,*args,**kwargs)
-class productDestroy(GenericAPIView,DestroyModelMixin ):
-    queryset = product.objects.all()
-    serializer_class = productSerializer
-
-    def delete(self,request,*args,**kwargs):
-        return self.destroy(request,*args,**kwargs)
